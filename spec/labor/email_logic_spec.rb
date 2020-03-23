03afb2ceff70c1aeebfc9ab001c619c5509084f3
Linda Le <yueling1222@gmail.com>
2020-03-22 20:37:01 -0600

    Add email logic spec notes

--- spec/labor/email_logic_spec.rb
+++ spec/labor/email_logic_spec.rb
@@ -1,12 +1,16 @@
 require "rails_helper"
 
 RSpec.describe EmailLogic, type: :labor do
+  # creates a user variable with factorybot
   let(:user) { create(:user) }
 
   describe "#analyze" do
     context "when user is brand new with no-follow" do
+      # ensures that this method returns a value of .5 if user has not opened enough emails yet or is new
       it "returns 0.5 for open_percentage" do
+        # sets a user as the the author
         author = create(:user)
+        # uses act_as_follower gem to set up inverse relationships to set up follow/following type relationships between models
         user.follow(author)
         create_list(:article, 3, user_id: author.id, positive_reactions_count: 20, score: 20)
         h = described_class.new(user).analyze
@@ -14,18 +18,21 @@ RSpec.describe EmailLogic, type: :labor do
       end
 
       it "provides top 3 articles" do
+        # would be better testing to create additional list with another few articles to ensure it really is returning the top three articles
         create_list(:article, 3, positive_reactions_count: 40, featured: true, score: 40)
         h = described_class.new(user).analyze
         expect(h.articles_to_send.length).to eq(3)
       end
 
-      it "marks as not ready if there isn't atleast 3 articles" do
+      it "marks as not ready if there isn't at least 3 articles" do
+        # list only creates 2 so this does check if there are at least 3 articles before creating email to send
         create_list(:article, 2, positive_reactions_count: 40, score: 40)
         h = described_class.new(user).analyze
         expect(h.should_receive_email?).to eq(false)
       end
 
       it "marks as not ready if there isn't at least 3 email-digest-eligible articles" do
+        # this test checks 4 articles with half not eligible for the email digest
         create_list(:article, 2, positive_reactions_count: 40, score: 40)
         create_list(:article, 2, positive_reactions_count: 40, email_digest_eligible: false)
         h = described_class.new(user).analyze
@@ -38,6 +45,7 @@ RSpec.describe EmailLogic, type: :labor do
         author = create(:user)
         user.follow(author)
         create_list(:article, 3, user_id: author.id, positive_reactions_count: 20, score: 20)
+        # sends out 10 emails to use for the following tests
         10.times do
           Ahoy::Message.create(mailer: "DigestMailer#digest_email",
                                user_id: user.id, sent_at: Time.current.utc)
@@ -45,6 +53,8 @@ RSpec.describe EmailLogic, type: :labor do
       end
 
       it "will not send email when user shouldn't receive any" do
+        # sent_at for the emails is set to current time
+        # after running through these methods, false will be returned
         h = described_class.new(user).analyze
         expect(h.should_receive_email?).to eq(false)
       end
@@ -62,6 +72,9 @@ RSpec.describe EmailLogic, type: :labor do
       end
 
       it "evaluates that user is ready to receive an email" do
+        # Timecop is used to freeze and time travel
+        # Any time that elaspes since tests are run will not impact the tests
+        # Should probably be removed and replaced with built in Rails functionality
         Timecop.freeze(3.days.from_now) do
           h = described_class.new(user).analyze
           expect(h.should_receive_email?).to eq(true)
@@ -72,6 +85,7 @@ RSpec.describe EmailLogic, type: :labor do
 
   describe "#should_receive_email?" do
     it "reflects @ready_to_receive_email" do
+      # These articles satisfy all of the helper methods in the analyze method so will return true
       author = create(:user)
       user.follow(author)
       create_list(:article, 3, user_id: author.id, positive_reactions_count: 20, score: 20)
