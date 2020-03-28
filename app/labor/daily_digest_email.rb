1528bba1b17810c8693112e6d1003e9c71c53196
Linda Le <yueling1222@gmail.com>
2020-03-28 12:28:50 -0600

    Add DailyDigestEmail model

--- app/labor/daily_digest_email.rb
+++ app/labor/daily_digest_email.rb
@@ -1,13 +1,13 @@
 class DailyDigestEmail
   def self.send_daily_digest_email(users = [])
-    new(users).send_daily_digest_email
+    new(users).send_daily_digest_emails
   end
 
   def initialize(users = [])
     @users = users.empty? ? get_users : users
   end
 
-  def send_daily_digest_email
+  def send_daily_digest_emails
     @users.find_each do |user|
       article = EmailLogic.new(user).analyze_daily_email
 
